4281e4f67ef070751dd1639c1e5842c0f1f7808f
KUBOTA Shota <ta2@o2p.jp>
2026-02-24 20:44:52 +0900

    feat: tweak SiteHeader UI

--- src/components/SiteHeader/SiteHeader.tsx
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -4,13 +4,14 @@ import Link from 'next/link';
 import { auth } from '../../features/auth/auth';
 import { SignInButton } from '../../features/auth/components/SignInButton';
 import { SignOutButton } from '../../features/auth/components/SignOutButton';
+import styles from './SiteHeader.module.css';
 
 export const SiteHeader = async (): Promise<React.JSX.Element> => {
   const session = await auth();
 
   return (
-    <header className="bg-primary-main flex items-center justify-between p-4">
-      <h1 className="text-2xl">
+    <header className={`${styles.header} flex items-center justify-between p-4`}>
+      <h1 className={`${styles.headings} text-2xl`}>
         <Link href="/" className="inline-flex items-center gap-2 text-monotone-100">
           <Image src="/images/icons/learn.svg" alt="" width={32} height={24} />
           omoikane
